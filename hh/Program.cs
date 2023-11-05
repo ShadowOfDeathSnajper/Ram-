c93a0f93c89ad6a6269d7a0876556e9dbf63a204
RamPZT <149948372+RamPZT@users.noreply.github.com>
2023-11-05 18:52:36 +0300

    Add files via upload

---
+++ hh/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+private void buttonExecute_Click(object sender, EventArgs e)
+{
+int result = 0;
+int number1, number2;
+int.TryParse(textBoxNumber1.Text, out number1);
+int.TryParse(textBoxNumber2.Text, out number2);
+
+switch (comboBoxSystem.Text)
+{
+case "Bin":
+switch (comboBoxOperation.Text)
+{
+case "+":
+result = number1 + number2;
+break;
+case "-":
+result = number1 — number2;
+break;
+case "*":
+result = number1 * number2;
+break;
+case "/":
+result = number1 / number2;
+break;
+}
+textBoxResult.Text = Convert.ToString(result, 2);
+break;
+
+case "Oct":
+switch (comboBoxOperation.Text)
+{
+case "+":
+result = number1 + number2;
+break;
+case "-":
+result = number1 — number2;
+break;
+case "*":
+result = number1 * number2;
+break;
+case "/":
+result = number1 / number2;
+break;
+}
+textBoxResult.Text = Convert.ToString(result, 8);
+break;
+
+case "Hex":
+switch (comboBoxOperation.Text)
+{
+case "+":
+result = number1 + number2;
+break;
+case "-":
+result = number1 — number2;
+break;
+case "*":
+result = number1 * number2;
+break;
+case "/":
+result = number1 / number2;
+break;
+}
+textBoxResult.Text = Convert.ToString(result, 16);
+break;
+}
+}
